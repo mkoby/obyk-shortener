469b82170ca2f9c94fb182247891f4f95aef4b8b
Michael Koby <michael@mkoby.com>
2012-08-20 13:06:23 -0500

    remove script from migration

--- db/migrate/20120820173629_change_short_code_in_short_link_table_to_binary_data_type.rb
+++ db/migrate/20120820173629_change_short_code_in_short_link_table_to_binary_data_type.rb
@@ -1,20 +1,7 @@
 class ChangeShortCodeInShortLinkTableToBinaryDataType < ActiveRecord::Migration
   def up
     change_table :short_links do |t|
-      short_code_hash = Hash.new
-      ShortLink.all.each do |s|
-        short_code_hash[s.id.to_s] = s.short_code
-        s.short_code = nil
-        s.save
-      end
-
       t.change :short_code, :binary
-
-      short_code_hash.each do |key, value|
-        s = ShortLink.find_by_id(key.to_i)
-        s.short_code = value
-        s.save
-      end
     end
   end
 
