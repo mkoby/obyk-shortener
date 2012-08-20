482209634730935aef9ebffc14ffaa0efd55afab
Michael Koby <michael@mkoby.com>
2012-08-20 12:58:03 -0500

    Add small script for updating short_codes to binary data type

--- db/migrate/20120820173629_change_short_code_in_short_link_table_to_binary_data_type.rb
+++ db/migrate/20120820173629_change_short_code_in_short_link_table_to_binary_data_type.rb
@@ -1,7 +1,19 @@
 class ChangeShortCodeInShortLinkTableToBinaryDataType < ActiveRecord::Migration
   def up
     change_table :short_links do |t|
+      short_code_hash = Hash.new
+      ShortLink.all.each do |s|
+        short_code_hash[s.id.to_s] = s.short_code
+        s.short_code = nil
+      end
+
       t.change :short_code, :binary
+
+      short_code_hash.each do |key, value|
+        s = ShortLink.find_by_id(key.to_i)
+        s.short_code = value
+        s.save
+      end
     end
   end
 
