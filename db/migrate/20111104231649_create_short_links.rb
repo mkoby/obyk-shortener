190cdcf90fbf3161f12f926da7d2d62d78ea18b5
Michael Koby <michael@mkoby.com>
2012-08-20 13:11:16 -0500

    change short_link to binary, get rid of migration that does this

--- db/migrate/20111104231649_create_short_links.rb
+++ db/migrate/20111104231649_create_short_links.rb
@@ -2,7 +2,7 @@ class CreateShortLinks < ActiveRecord::Migration
   def change
     create_table :short_links do |t|
       t.string :full_link
-      t.string :short_code
+      t.binary :short_code
 
       t.timestamps
     end
